5b11e650b0ba4427e6dadf33929a355b8031d589
Stivi-Kiwi <StevenPlanck@web.de>
2023-02-17 18:07:03 +0100

    Updated content

--- js/scroll.js
+++ js/scroll.js
@@ -3,10 +3,10 @@ let activeSection = 0;
 
 document.addEventListener("wheel", function(event) {
     if (event.deltaY > 0) {
-        activeSection = Math.min(activeSection + 1, sections.length - 1);
+        activeSection = Math.min (activeSection + 1, sections.length -1);
     } else if (event.deltaY < 0) {
         activeSection = Math.max(activeSection - 1, 0);
     }
-
+    
     window.location = "#" + sections[activeSection].id;
 });
